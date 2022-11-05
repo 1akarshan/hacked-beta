3360591dfec6ad91ba615c99ffb437c8ce86ad7e
Fardeen Faisal <68156295+fardeenfs@users.noreply.github.com>
2022-11-05 13:21:31 -0600

    Undo Changes (Wrong Branch)

--- backend/imageprocessing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import VideoCaptureViewSet
-
-
-urlpatterns = [
-
-    path('upload/', VideoCaptureViewSet.as_view()),
-]
